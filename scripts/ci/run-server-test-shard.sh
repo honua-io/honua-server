22486f14b2e3da1ca2cdf36bc1e80cc2a211aea8
makani <mike@honua.io>
2026-05-26 08:20:41 -1000

    ci: scope fast test exclusion to ci shards

--- scripts/ci/run-server-test-shard.sh
+++ scripts/ci/run-server-test-shard.sh
@@ -24,7 +24,7 @@ heartbeat_seconds="${HONUA_SERVER_TEST_HEARTBEAT_SECONDS:-30}"
 heartbeat_tail_lines="${HONUA_SERVER_TEST_HEARTBEAT_TAIL_LINES:-40}"
 console_verbosity="${HONUA_SERVER_TEST_CONSOLE_VERBOSITY:-normal}"
 exclude_slow="${HONUA_SERVER_TEST_EXCLUDE_SLOW:-true}"
-exclude_fast="${HONUA_SERVER_TEST_EXCLUDE_FAST:-true}"
+exclude_fast="${HONUA_SERVER_TEST_EXCLUDE_FAST:-false}"
 
 mkdir -p "${results_dir}"
 
