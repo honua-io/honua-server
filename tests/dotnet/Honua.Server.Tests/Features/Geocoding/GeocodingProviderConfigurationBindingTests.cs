beea6453669fe075a612f172e07080a10687e810
Mike McDougall <mike@honua.io>
2026-07-24 05:21:14 -1000

    fix(geocoding): remove unused binding test import

--- tests/dotnet/Honua.Server.Tests/Features/Geocoding/GeocodingProviderConfigurationBindingTests.cs
+++ tests/dotnet/Honua.Server.Tests/Features/Geocoding/GeocodingProviderConfigurationBindingTests.cs
@@ -2,7 +2,6 @@
 // Licensed under the Elastic License 2.0. See LICENSE in the project root.
 
 using Honua.Geocoding.Features.Geocoding.Domain;
-using Honua.Geocoding.Features.Geocoding.Integration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
