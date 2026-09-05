16cc089b593ba96fba13ee82f549922638337850
Mike McDougall <mike@honua.io>
2026-09-04 19:39:13 -1000

    wip: preserve MapLibre startup and browser test evidence

--- tests/js-browser/playwright.maplibre.config.ts
+++ tests/js-browser/playwright.maplibre.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   workers: 1,
   reporter: [
     ['list'],
+    ['junit', { outputFile: 'test-results/maplibre-junit.xml' }],
     ['./maplibre/support/cert-reporter.ts'],
   ],
   globalSetup: './maplibre/global-setup.ts',
