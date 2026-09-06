c7924f75a2295ab46a5f458f67d19e618d3a1628
Mike McDougall <mike@honua.io>
2026-09-05 16:52:36 -1000

    test: fix theory import and validate worker rebase (#4141)

--- tests/dotnet/Honua.Worker.Gdal.Tests/GdalWorkerLicensingTests.cs
+++ tests/dotnet/Honua.Worker.Gdal.Tests/GdalWorkerLicensingTests.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using StackExchange.Redis;
+using Xunit;
 
 namespace Honua.Worker.Gdal.Tests;
 
